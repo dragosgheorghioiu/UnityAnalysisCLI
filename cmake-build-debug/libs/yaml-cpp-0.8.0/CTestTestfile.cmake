92619ae289d07c9acc3c620eb0ab748b83ea8f73
Dragos Gheorghioiu <73996904+dragosgheorghioiu@users.noreply.github.com>
2023-12-10 03:21:20 +0200

    Script dump (#2)
    * generating csv with unused scripts
    
    * small cleanup

--- cmake-build-debug/libs/yaml-cpp-0.8.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/drago/CLionProjects/UnityAnalysisCLI/libs/yaml-cpp-0.8.0
-# Build directory: C:/Users/drago/CLionProjects/UnityAnalysisCLI/cmake-build-debug/libs/yaml-cpp-0.8.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
