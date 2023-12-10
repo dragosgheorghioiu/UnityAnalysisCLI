49ba851112b47db40a7e14b585341797c591cb1f
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2023-12-10 16:52:26 +0200

    parallelism with OpenMP

---
+++ libs/yaml-cpp-0.8.0/util/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/drago/CLionProjects/UnityAnalysisCLI/libs/yaml-cpp-0.8.0/util
+# Build directory: C:/Users/drago/CLionProjects/UnityAnalysisCLI/libs/yaml-cpp-0.8.0/util
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "Process.h"
+#include "src/Process.h"
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        std::cout << "Not enough params provided (Ex: tool.exe path/to/input path/to/output)" << std::endl;
+        std::cerr << "Not enough params provided (Ex: tool.exe path/to/input path/to/output)" << std::endl;
         return 0;
     }
     Process(argc, argv).run();
