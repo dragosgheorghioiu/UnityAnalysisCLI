b2a4178b9f6daf10e6233122b096ca25b54e8d3c
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2023-12-10 18:09:34 +0200

    README

---
+++ build/libs/yaml-cpp-0.8.0/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/drago/CLionProjects/UnityAnalysisCLI/libs/yaml-cpp-0.8.0
+# Build directory: C:/Users/drago/CLionProjects/UnityAnalysisCLI/build/libs/yaml-cpp-0.8.0
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("util")
